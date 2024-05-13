ecdb9e0553a96ecda7c438a5f9cee846a1b9551e
rstiskalek <richard.stiskalek@protonmail.com>
2024-05-13 17:45:50 +0100

    Get rid of GSL implentation of Bessel

--- src/PecVecCov.jl
+++ src/PecVecCov.jl
@@ -1,6 +1,6 @@
 module PecVecCov
 
-import GSL: sf_bessel_jl, sf_legendre_Pl
+import GSL: sf_legendre_Pl
 import Interpolations: LinearInterpolation
 import NPZ: npzread
 import Integrals: SimpsonsRule, SampledIntegralProblem, solve
@@ -14,15 +14,7 @@ export build_Pk_interpolator, djn, sf_legendre_Pl, C_ij, djn2
 
 Compute the derivative of the spherical Bessel function of the first kind `jₙ(x)` with respect to `x`.
 """
-djn(n, x) = n / x * sf_bessel_jl(n, x) - sf_bessel_jl(n + 1, x)
-
-
-"""
-    djn2(n::Integer, x::Number) -> Number
-
-More numerically stable version of `djn` but slower.
-"""
-djn_stable(n, x) = n / x * sphericalbesselj(n, x) - sphericalbesselj(n + 1, x)
+djn(n, x) = n / x * sphericalbesselj(n, x) - sphericalbesselj(n + 1, x)
 
 
 """
@@ -53,11 +45,7 @@ function C_ij(ri, rj, cosθ, Pk, ks; ell_min=0, ell_max=20)
         kri = ks[i] * ri
         krj = ks[i] * rj
         for ell in ell_min:ell_max
-            if ell > 50
-                ys_dummy[i] += (ell + 1) * djn_stable(ell, kri) * djn_stable(ell, krj) * sf_legendre_Pl(ell, cosθ)
-            else
-                ys_dummy[i] += (ell + 1) * djn(ell, kri) * djn(ell, krj) * sf_legendre_Pl(ell, cosθ)
-            end
+            ys_dummy[i] += (ell + 1) * djn(ell, kri) * djn(ell, krj) * sf_legendre_Pl(ell, cosθ)
         end
 
         ys_dummy[i] *= Pk[i]
