dc31426ffb31e5014731305577fed103195fe2bc
rstiskalek <richard.stiskalek@protonmail.com>
2024-05-16 11:24:47 +0100

    Add grid sampling

---
+++ scripts/sample_grid_Cii.jl
@@ -0,0 +1,55 @@
+#=
+Script to sample the covariance matrix C_ii over a grid of r = r' and cosθ = 1 values.
+The underlying cosmological choice is banked in through the power spectrum interpolator.
+The script saves the computed covariance matrix elements.
+=#
+using PecVelCov
+using ProgressMeter
+using Base.Threads
+using JLD2
+
+include_dipole = false
+ell_min = include_dipole ? 1 : 2
+ell_max = 100
+
+# Define the grid over which to sample the covariance matrix, and the k values
+rs = LinRange(0.1, 350, 50000)  # Mpc / h
+ks = 10 .^ LinRange(-4, 1, 2 * 2048)
+println("Sampling diagonal covariance matrix elements over $(length(rs)) grid points.")
+println("Spacing in r: $(rs[2] - rs[1]) Mpc / h")
+
+# Load the power spectrum interpolator
+fname_pk = "/mnt/users/rstiskalek/BayesianBulkFlows/data/pk_fiducial.npy"
+println("Loading power spectrum from `$fname_pk`...")
+pk_interp = build_Pk_interpolator(fname_pk)
+pk = pk_interp(ks)
+println("Power spectrum loaded. Ranges from $(minimum(ks)) to $(maximum(ks)).")
+
+
+# Precompute the derivatives of the spherical Bessel functions
+println("Precomuting the derivatives of the spherical Bessel functions...")
+kmin, kmax = minimum(ks), maximum(ks)
+rmin, rmax = minimum(rs), maximum(rs)
+krs = 10 .^ LinRange(log10(kmin * rmin), log10(kmax * rmax), 262144)
+djn_interp = precompute_djn(ell_min, ell_max, krs);
+start_krs = precompute_djn_start(djn_interp, krs)
+
+nr = length(rs)
+Cii_grid = zeros(nr)
+# The Legendre polynomials for cosθ = 1, i.e. all are just 1.
+Pells = Dict(ell => 1. for ell in ell_min:ell_max)
+
+println("We are running with $(Threads.nthreads()) threads.\n")
+
+@showprogress dt=1 @threads for i in 1:nr
+    kri = ks .* rs[i]
+    Cii_grid[i] = C_ij(kri, kri, Pells, pk, ks; ell_max=ell_max, djn_interp=djn_interp, start_krs=start_krs)
+end
+
+
+fname_out = "/mnt/extraspace/rstiskalek/BBF/Cii_grid.jld2"
+if include_dipole
+    fname_out = replace(fname_out, ".jld2" => "_including_dipole.jld2")
+end
+println("Saving computed covariance matrix elements to `$fname_out`.")
+jldsave(fname_out, rs=rs, Cii_grid=Cii_grid)
