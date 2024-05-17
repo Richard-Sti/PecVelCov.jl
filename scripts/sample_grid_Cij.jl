6bff6c21745379dbde335314bf4a160a725531e1
Richard Stiskalek <richard.stiskalek@protonmail.com>
2024-05-17 18:42:01 +0100

    Speed up C_ij (#1)
    * Speed up covariance matrix building
    
    * Update README
    
    * Add further speed ups with start krs
    
    * Remove reversing
    
    * Update script for more speed
    
    * Rename function
    
    * Update .gitignore
    
    * Update project
    
    * Add new brute and speed calculations
    
    * Update settings
    
    * Rename file
    
    * Add comments
    
    * Update how comment is written
    
    * Add C_ii interpolator
    
    * Add grid sampling
    
    * Update Pk to vector operatoin
    
    * Add script to precompute djn
    
    * Set ell_max default to 100
    
    * Update README
    
    * Turn to a single script
    
    * Remove old file
    
    * Small edits..
    
    * Add docs
    
    * Rename function
    
    * Add some blanks
    
    * Update script
    
    * Add joint interpolator
    
    * Quick fix bug
    
    * Update README
    
    * Add temporary saving
    
    * Split calculation
    
    * Update README

---
+++ scripts/sample_grid_Cij.jl
@@ -0,0 +1,220 @@
+#=
+Script to sample the covariance matrix C_ij over a grid of r, r', and cosθ values. The underlying cosmological
+choice is banked in through the power spectrum interpolator. The script saves the computed covariance matrix elements.
+=#
+using ArgParse
+using Base.Threads
+using JLD2
+using PecVelCov
+using ProgressMeter
+
+
+###############################################################################
+#                          Parse the command line                             #
+###############################################################################
+
+
+function get_cmd(args)
+    s = ArgParseSettings()
+    @add_arg_table! s begin
+        "--runtype"
+            help = "Type of run: `full`, `diagonal`, `close`, or `opposite"
+            arg_type = String
+        "--continue"
+            help = "Whether to continue the computation from the last saved point."
+            arg_type = Bool
+            default = false
+        "--include_dipole"
+            help = "Include the dipole term in the covariance matrix"
+            arg_type = Bool
+            default = false
+        "--logk_range"
+            help = "Range of log(k) values [h / Mpc]"
+            arg_type = Float64
+            nargs = 2
+            default = [-4., 1.]
+        "--npoints_k"
+            help = "Number of points in k"
+            arg_type = Int
+            default = 4096
+    end
+
+    args = parse_args(args, s)
+
+    args["fname_pk"] = "/mnt/users/rstiskalek/BayesianBulkFlows/data/pk_fiducial.npy"
+    args["fname_djn"] = "/mnt/extraspace/rstiskalek/BBF/djn_grid.jld2"
+    args["ell_min"] = args["include_dipole"] ? 1 : 2
+
+    if args["runtype"] == "full"
+        args["fname_out"] = "/mnt/extraspace/rstiskalek/BBF/Cij_grid.jld2"
+        args["rs"] = LinRange(0.1, 350, 500)  # Mpc / h
+        args["cosθs"] = LinRange(-0.93, 0.93, 500)
+        args["ell_max"] = 20
+        println("Sampling Σ_ij over $(length(args["rs"])) x $(length(args["rs"])) x $(length(args["cosθs"])) grid points.")
+    elseif args["runtype"] == "diagonal"
+        args["fname_out"] = "/mnt/extraspace/rstiskalek/BBF/Cii_grid.jld2"
+        args["rs"] = LinRange(0.1, 350, 50000)  # Mpc / h
+        args["cosθs"] = nothing
+        args["ell_max"] = 100
+        println("Sampling Σ_ii over $(length(args["rs"])) grid points.")
+    elseif args["runtype"] == "close"
+        args["fname_out"] = "/mnt/extraspace/rstiskalek/BBF/Cij_close_grid.jld2"
+        args["rs"] = LinRange(0.1, 350, 500)  # Mpc / h
+        args["cosθs"] = LinRange(0.925, 1, 500)
+        args["ell_max"] = 100
+        println("Sampling Σ_ij over $(length(args["rs"])) x $(length(args["rs"])) x $(length(args["cosθs"])) grid points.")
+    elseif args["runtype"] == "opposite"
+        args["fname_out"] = "/mnt/extraspace/rstiskalek/BBF/Cij_opposite_grid.jld2"
+        args["rs"] = LinRange(0.1, 350, 500)  # Mpc / h
+        args["cosθs"] = LinRange(-1, -0.925, 500)
+        args["ell_max"] = 100
+        println("Sampling Σ_ij over $(length(args["rs"])) x $(length(args["rs"])) x $(length(args["cosθs"])) grid points.")
+    else
+        error("Invalid runtype: $(args["runtype"])")
+    end
+
+    println("Spacing in r: $(args["rs"][2] - args["rs"][1]) [Mpc / h].")
+    if args["cosθs"] !== nothing
+        println("Spacing in cosθ: $(args["cosθs"][2] - args["cosθs"][1]).")
+    end
+
+    if args["include_dipole"]
+        args["fname_out"] = replace(args["fname_out"], ".jld2" => "_including_dipole.jld2")
+    end
+
+    args["fname_temp"] = replace(args["fname_out"], ".jld2" => "_temp.jld2")
+
+    flush(stdout)
+    return args
+end
+
+
+###############################################################################
+#                          Main computation functions                         #
+###############################################################################
+
+
+function run_full(rs, cosθs, Pk, ks, ell_min, ell_max, djn_interp, start_krs, fname_temp)
+    nr, ncosθ = length(rs), length(cosθs)
+
+    kstart = 1
+    jldopen(fname_temp, "r") do file
+        kstart = file["kstart"]
+        if kstart > 1
+            println("Resuming computation from k = $(kstart)."); flush(stdout)
+        end
+    end
+
+    p = Progress(nr * nr * ncosθ, dt=1)
+    for k in kstart:ncosθ
+        cosθ = cosθs[k]
+        Pells = precompute_legendre_Pells(ell_min, ell_max, cosθ)
+        Cij_current = fill(NaN, nr, nr)
+        # Parallelize the inner loop for thread safety when writing the temporary file.
+        @threads for i in 1:nr
+            kri = ks .* rs[i]
+            for j in 1:nr
+                krj = ks .* rs[j]
+                Cij_current[i, j] = C_ij(kri, krj, Pells, Pk, ks; ell_max=ell_max, djn_interp=djn_interp, start_krs=start_krs)
+                next!(p)
+            end
+            flush(stdout)
+        end
+
+        # Save the computed elements to a temporary file in case the computation is interrupted.
+        jldopen(fname_temp, "r+") do file
+            file["Σ_$k"] = Cij_current
+            delete!(file, "kstart")
+            file["kstart"] = k + 1
+        end
+
+    end
+    finish!(p)
+
+    # Now concatenate the computed elements from the temporary files into a single array.
+    Cij_grid = zeros(nr, nr, ncosθ)
+    jldopen(fname_temp, "r") do file
+        for k in 1:ncosθ
+            Cij_grid[:, :, k] = file["Σ_$k"]
+        end
+    end
+
+    return Cij_grid
+end
+
+
+function run_diagonal(rs, Pk, ks, ell_min, ell_max, djn_interp, start_krs)
+    nr = length(rs)
+    Cii_grid = zeros(nr)
+
+    Pells = Dict(ell => 1. for ell in ell_min:ell_max)
+
+    @showprogress dt=1 @threads for i in 1:nr
+        kri = ks .* rs[i]
+        Cii_grid[i] = C_ij(kri, kri, Pells, Pk, ks; ell_max=ell_max, djn_interp=djn_interp, start_krs=start_krs)
+    end
+
+    return Cii_grid
+end
+
+
+###############################################################################
+#                                 Main script                                #
+###############################################################################
+
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    println("We are running with $(Threads.nthreads()) threads.\n")
+    args = get_cmd(ARGS)
+
+    # Diagonal is fast so we don't need to ever continue it.
+    if args["continue"] && args["runtype"] == "diagonal"
+        error("Cannot continue the computation for the diagonal elements.")
+    end
+
+    if args["continue"]
+        if !isfile(args["fname_temp"])
+            error("File `$(args["fname_out"])` does not exist.")
+        end
+
+    else
+        if isfile(args["fname_temp"])
+            println("File `$(args["fname_temp"])` already exists. Removing it.")
+            rm(args["fname_temp"])
+        end
+
+        # Write a new temporary file
+        jldopen(args["fname_temp"], "w") do file
+            file["rs"] = args["rs"]
+            file["cosθs"] = args["cosθs"]
+            file["kstart"] = 1
+        end
+    end
+
+    println("Loading power spectrum from `$(args["fname_pk"])`..."), flush(stdout)
+    ks = 10 .^ LinRange(args["logk_range"][1], args["logk_range"][2], args["npoints_k"])
+    Pk = build_Pk_interpolator(args["fname_pk"]).(ks)
+
+    println("Loading the precomputed spherical Bessel function derivatives from `$(args["fname_djn"])`..."), flush(stdout)
+    djn_interp, start_krs = build_dnj_interpolator(args["fname_djn"])
+
+
+    println("Starting computation, kind is `$(args["runtype"])`."), flush(stdout)
+    if args["runtype"] == "diagonal"
+        Σ = run_diagonal(args["rs"], Pk, ks, args["ell_min"], args["ell_max"], djn_interp, start_krs)
+    else
+        Σ = run_full(args["rs"], args["cosθs"], Pk, ks, args["ell_min"], args["ell_max"], djn_interp, start_krs, args["fname_temp"])
+    end
+
+    println("Saving computed covariance matrix elements to `$(args["fname_out"])`.")
+    jldopen(args["fname_out"], "w") do file
+        file["rs"] = args["rs"]
+        file["cosθs"] = args["cosθs"]
+        file["Σ"] = Σ
+    end
+
+    if isfile(args["fname_temp"])
+        println("Removing temporary file `$(args["fname_temp"])`.")
+        rm(args["fname_temp"])
+    end
+end
--- src/PecVelCov.jl
+++ src/PecVelCov.jl
@@ -5,11 +5,13 @@ import Integrals: SampledIntegralProblem, SimpsonsRule, solve
 import JLD2: jldopen
 import NPZ: npzread
 import SpecialFunctions: sphericalbesselj
+using ProgressMeter
 using LinearAlgebra
 using Interpolations
 
-export build_Cij_interpolator, build_Pk_interpolator, covariance_inverse_and_determinant,
-       C_ij, pecvel_covariance_matrix
+export build_Cij_interpolator, build_Cij_joint_interpolator, build_Cii_interpolator, build_dnj_interpolator,
+    build_Pk_interpolator, covariance_inverse_and_determinant, C_ij, djn, sf_legendre_Pl,
+    precompute_legendre_Pells, pecvel_covmat_from_interp, pecvel_covmat_brute
 
 
 ###############################################################################
@@ -21,13 +23,15 @@ export build_Cij_interpolator, build_Pk_interpolator, covariance_inverse_and_det
     build_Pk_interpolator(fname::String) -> LinearInterpolation
 
 Build a linear interpolator for the power spectrum `P(k)` from the data stored in the file `fname`.
+Internally, the interpolator is built in log-log space.
 """
 function build_Pk_interpolator(fname)
     X = npzread(fname)
     k = X[:, 1]
     Pk = X[:, 2]
 
-    return LinearInterpolation(k, Pk)
+    finterp = LinearInterpolation(log.(k), log.(Pk))
+    return x -> exp(finterp(log(x)))
 end
 
 
@@ -41,15 +45,88 @@ function build_Cij_interpolator(fname)
     jldopen(fname, "r") do file
         rs = file["rs"]
         cosθs = file["cosθs"]
-        Cij_grid = file["Cij_grid"]
+        Cij_grid = file["Σ"]
     end
 
     interp = interpolate(Cij_grid, BSpline(Cubic(Line(OnGrid()))))
     interp = scale(interp, rs, rs, cosθs)
-    interp = extrapolate(interp, Line())
-
-    return interp
+    return extrapolate(interp, Line())
 end
+
+
+"""
+    build_Cij_joint_interpolator(fname_full::String, fname_close::String, fname_opposite::String) -> Function
+
+Build a joint interpolator for the covariance matrix elements `C_{ij}` from the data stored in the files `fname_full`,
+`fname_close`, and `fname_opposite`. Choice of the appropriate interpolator is based on the hard-coded value of `cosθ`.
+"""
+function build_Cij_joint_interpolator(fname_full, fname_close, fname_opposite)
+    Cij_interp_full = build_Cij_interpolator(fname_full)
+    Cij_interp_close = build_Cij_interpolator(fname_close)
+    Cij_interp_opposite = build_Cij_interpolator(fname_opposite)
+
+    function Cij_joint_interp(r1, r2, cosθ)
+        if cosθ > 0.925
+            return Cij_interp_close(r1, r2, cosθ)
+        elseif cosθ < -0.925
+            return Cij_interp_opposite(r1, r2, cosθ)
+        else
+            return Cij_interp_full(r1, r2, cosθ)
+        end
+    end
+
+    return Cij_joint_interp
+
+end
+
+
+"""
+    build_Cii_interpolator(fname::String) -> Interpolations.ScaledInterpolation
+
+Build an interpolator for the covariance matrix diagonal elements `C_{ii}` from the data stored in the file `fname`.
+"""
+function build_Cii_interpolator(fname)
+    rs, Cii_grid = nothing, nothing, nothing
+    jldopen(fname, "r") do file
+        rs = file["rs"]
+        Cii_grid = file["Σ"]
+    end
+
+    interp = interpolate(Cii_grid, BSpline(Cubic(Line(OnGrid()))))
+    interp = scale(interp, rs)
+    return extrapolate(interp, Line())
+end
+
+
+"""
+    precompute_legendre_Pells(ell_min, ell_max, cosθ) -> Dict
+
+Precompute the Legendre polynomials for the range of multipoles `ell_min` to `ell_max` at `cosθ`.
+"""
+function precompute_legendre_Pells(ell_min, ell_max, cosθ)
+    return Dict(ell => sf_legendre_Pl(ell, cosθ) for ell in ell_min:ell_max)
+end
+
+
+"""
+    build_dnj_interpolator(fname::String) -> Tuple{Dict, Dict}
+
+Build a precomputed interpolator for the derivative of the spherical Bessel function of the first kind
+`jₙ(x)` with respect to `x` for the range of multipoles `ell_min` to `ell_max`.
+"""
+function build_dnj_interpolator(fname)
+    xs, ells, ys = nothing, nothing, nothing
+    jldopen(fname, "r") do file
+        xs = file["xs"]
+        ells = file["ells"]
+        ys = file["ys"]
+    end
+
+    dnj_interp = Dict(ell => interpolate((xs,), ys[:, i], Gridded(Linear())) for (i, ell) in enumerate(ells))
+    start_xs = Dict(ell => xs[findfirst(x -> abs(x) > 1e-10, ys[:, i])] for (i, ell) in enumerate(ells))
+    return dnj_interp, start_xs
+end
+
 
 ###############################################################################
 #                   Covariance matrix elements calculation                    #
@@ -59,33 +136,40 @@ end
 """
     djn(n::Integer, x::Number) -> Number
 
-Compute the derivative of the spherical Bessel function of the first kind `jₙ(x)` with respect to `x`.
+Derivative of the spherical Bessel function of the first kind `jₙ(x)` with respect to `x`.
 """
 djn(n, x) = n / x * sphericalbesselj(n, x) - sphericalbesselj(n + 1, x)
 
 
 """
-    C_ij(ri, rj, cosθ, Pk, ks; ell_min=0, ell_max=20) -> Number
+    C_ij(kri, krj, Pells, Pk, ks; ell_min=2, ell_max=20, djn_interp=nothing, start_krs=nothing) -> Number
 
-Calculate the covariance matrix element `C_{ij}` for the two points `rᵢ` and `rⱼ` separated by
+Covariance matrix element `C_{ij}` for the two points `rᵢ` and `rⱼ` separated by
 an angle `θ`.
 
 # TODO: Still missing prefactors before the integral over k.
 """
-function C_ij(ri, rj, cosθ, Pk, ks; ell_min=0, ell_max=20)
+function C_ij(kri, krj, Pells, Pk, ks; ell_min=2, ell_max=20, djn_interp=nothing, start_krs=nothing)
     ys_dummy = zeros(length(ks))
-    for i in eachindex(ks)
-        kri = ks[i] * ri
-        krj = ks[i] * rj
-        for ell in ell_min:ell_max
-            ys_dummy[i] += (ell + 1) * djn(ell, kri) * djn(ell, krj) * sf_legendre_Pl(ell, cosθ)
-        end
+    ell_range = ell_min:ell_max
+    djn_eval = isa(djn_interp, Nothing) ? (ell, k) -> djn(ell, k) : (ell, k) -> djn_interp[ell](k)
+    start_krs_eval = isa(start_krs, Nothing) ? ell -> 0.0 : ell -> start_krs[ell]
 
-        ys_dummy[i] *= Pk[i]
+    @inbounds for i in eachindex(ks)
+        y_val, kri_i, krj_i = 0.0, kri[i], krj[i]
+        for ell in ell_range
+            # Once this condition is trigerred it will always be trigerred for
+            # the rest of the loop over ell.
+            start_kr = start_krs_eval(ell)
+            if kri_i < start_kr || krj_i < start_kr
+                break
+            end
+            y_val += @fastmath (ell + 1) * djn_eval(ell, kri_i) * djn_eval(ell, krj_i) * Pells[ell]
+        end
+        ys_dummy[i] = y_val * Pk[i]
     end
 
-    sol = solve(SampledIntegralProblem(ys_dummy, ks), SimpsonsRule())
-    return sol.u
+    return solve(SampledIntegralProblem(ys_dummy, ks), SimpsonsRule()).u
 end
 
 
@@ -95,40 +179,76 @@ end
 
 
 """
-    pecvel_covariance_matrix(rs, θs, ϕs; Cij_interpolator=nothing) -> Matrix
+    pecvel_covmat_from_interp(rs, θs, ϕs, Pk, ks, Cij_interpolator, Cii_interpolator; ell_min, djn_interp, start_krs) -> Matrix
 
 Compute the covariance matrix for the peculiar velocity field for a set of tracers using the
-interpolator `Cij_interpolator` for the covariance matrix elements.
+interpolator `Cij_interpolator` for the off-diagonal covariance matrix elements and `Cii_interpolator`
+for the diagonal elements.
 """
-function pecvel_covariance_matrix(rs, θs, ϕs; Cij_interpolator=nothing)
-    if isa(Cij_interpolator, String)
-        Cij_interpolator = build_Cij_interpolator(Cij_interpolator)
-    end
-
-    # Check if valid interpolator is provided.
-    if isa(Cij_interpolator, Nothing)
-        error("Either provide `Cij_interpolator`` or a filename to build it.")
-    end
-
+function pecvel_covmat_from_interp(rs, θs, ϕs, Pk, ks, Cij_interpolator, Cii_interpolator; ell_min, djn_interp, start_krs)
     @assert length(rs) == length(θs) && length(θs) == length(ϕs) "rs, θs, and ϕs must have the same length."
-    sinθs = sin.(θs)
-    cosθs = cos.(θs)
+    sinθs, cosθs = sin.(θs), cos.(θs)
 
     Σ = zeros(length(rs), length(rs))
+    # First compute the off-diagonal elements.
+    @showprogress dt = 0.1 for i in eachindex(rs)
+        sinθ_i, cosθ_i, ϕ_i = sinθs[i], cosθs[i], ϕs[i]
+        for j in eachindex(rs)
+            if j > i
+                continue
+            end
 
-    for i in eachindex(rs), j in eachindex(rs)
-        if j > i
-            continue
+            cosΔ = min(sinθ_i * sinθs[j] * cos(ϕ_i - ϕs[j]) + cosθ_i * cosθs[j], 1.0)
+            Σij = Cij_interpolator(rs[i], rs[j], cosΔ)
+
+            Σ[i, j] = Σij
+            Σ[j, i] = Σij
         end
+    end
 
-        cosΔ = sinθs[i] * sinθs[j] * cos(ϕs[i] - ϕs[j]) + cosθs[i] * cosθs[j]
-        Σij = Cij_interpolator(rs[i], rs[j], cosΔ)
-
-        Σ[i, j] = Σij
-        Σ[j, i] = Σij
+    # And then compute the diagonal elements.
+    for i in eachindex(rs)
+        Σ[i, i] = Cii_interpolator(rs[i])
     end
 
     return Σ
+end
+
+
+"""
+    pecvel_covariance_matrix_brute(rs, θs, ϕs, Pk, ks; ell_min=2, djn_interp=nothing, start_krs=nothing) -> Matrix
+
+Compute the covariance matrix for the peculiar velocity field for a set of tracers using a brute-force, without
+any interpolation of C_ij elements.
+"""
+function pecvel_covmat_brute(rs, θs, ϕs, Pk, ks; ell_min=2, djn_interp=nothing, start_krs=nothing)
+    @assert length(rs) == length(θs) && length(θs) == length(ϕs) "rs, θs, and ϕs must have the same length."
+    sinθs, cosθs = sin.(θs), cos.(θs)
+
+    Σ = zeros(length(rs), length(rs))
+    @showprogress dt = 0.1 for i in eachindex(rs)
+        kri = ks .* rs[i]
+        sinθ_i, cosθ_i = sinθs[i], cosθs[i]
+        ϕ_i = ϕs[i]
+
+        for j in eachindex(rs)
+            if j > i
+                continue
+            end
+
+            krj = ks .* rs[j]
+            cosΔ = min(sinθ_i * sinθs[j] * cos(ϕ_i - ϕs[j]) + cosθ_i * cosθs[j], 1.0)
+
+            ell_max = cosΔ < 0.95 ? 20 : 100
+            Pells = precompute_legendre_Pells(ell_min, ell_max, cosΔ)
+
+            Σij = C_ij(kri, krj, Pells, Pk, ks; ell_max=ell_max, djn_interp=djn_interp, start_krs=start_krs)
+            Σ[i, j] = Σij
+            Σ[j, i] = Σij
+        end
+    end
+    return Σ
+
 end
 
 
